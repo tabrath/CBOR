f473b97c2e5d6ba2c894ef2f90c666a2f7cdceab
Trond Bråthen <tabrath@gmail.com>
2017-05-27 01:50:24 +0200

    formatting

--- src/PeterO.Cbor/BigInteger.cs
+++ src/PeterO.Cbor/BigInteger.cs
@@ -10,118 +10,84 @@ using System;
 using PeterO.Numbers;
 
 namespace PeterO {
-    /// <include file='../docs.xml'
-    /// path='docs/doc[@name="T:PeterO.BigInteger"]/*'/>
-[Obsolete(
-  "Use EInteger from PeterO.Numbers/com.upokecenter.numbers and the output of this class's ToString method.")]
-  public sealed partial class BigInteger : IComparable<BigInteger>,
-    IEquatable<BigInteger> {
+  /// <include file='../docs.xml'
+  /// path='docs/doc[@name="T:PeterO.BigInteger"]/*'/>
+  [Obsolete("Use EInteger from PeterO.Numbers/com.upokecenter.numbers and the output of this class's ToString method.")]
+  public sealed partial class BigInteger : IComparable<BigInteger>, IEquatable<BigInteger>
+  {
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="F:PeterO.BigInteger.ONE"]/*'/>
 #if CODE_ANALYSIS
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
-      "Microsoft.Security",
-      "CA2104",
-      Justification = "BigInteger is immutable")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104", Justification = "BigInteger is immutable")]
 #endif
 
     [Obsolete("Use EInteger from PeterO.Numbers/com.upokecenter.numbers.")]
     public static readonly BigInteger ONE = new BigInteger(EInteger.One);
 
-    private static readonly BigInteger ValueOneValue = new
-      BigInteger(EInteger.One);
+    private static readonly BigInteger ValueOneValue = new BigInteger(EInteger.One);
 
     private readonly EInteger ei;
 
     internal BigInteger(EInteger ei) {
-      if (ei == null) {
-  throw new ArgumentNullException("ei");
-}
+      if (ei == null)
+        throw new ArgumentNullException(nameof(ei));
+
       this.ei = ei;
     }
 
-    internal static BigInteger ToLegacy(EInteger ei) {
-      return new BigInteger(ei);
-    }
+    internal static BigInteger ToLegacy(EInteger ei) => new BigInteger(ei);
 
-    internal static EInteger FromLegacy(BigInteger bei) {
-      return bei.Ei;
-    }
+    internal static EInteger FromLegacy(BigInteger bei) => bei.Ei;
 
-  private static readonly BigInteger ValueZeroValue = new
-      BigInteger(EInteger.Zero);
+    private static readonly BigInteger ValueZeroValue = new BigInteger(EInteger.Zero);
 
-    internal EInteger Ei {
-      get {
-        return this.ei;
-      }
-    }
+    internal EInteger Ei => this.ei;
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.fromBytes(System.Byte[],System.Boolean)"]/*'/>
-  public static BigInteger fromBytes(byte[] bytes, bool littleEndian) {
-      return new BigInteger(EInteger.FromBytes(bytes, littleEndian));
-    }
+    public static BigInteger fromBytes(byte[] bytes, bool littleEndian) => new BigInteger(EInteger.FromBytes(bytes,   littleEndian));
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.fromRadixString(System.String,System.Int32)"]/*'/>
-  public static BigInteger fromRadixString(string str, int radix) {
-      return new BigInteger(EInteger.FromRadixString(str, radix));
-    }
+    public static BigInteger fromRadixString(string str, int radix) =>  new BigInteger(EInteger.FromRadixString(str, radix));
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.fromString(System.String)"]/*'/>
-  public static BigInteger fromString(string str) {
-return new BigInteger(EInteger.FromString(str));
-}
+    public static BigInteger fromString(string str) => new BigInteger(EInteger.FromString(str));
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.valueOf(System.Int64)"]/*'/>
-  public static BigInteger valueOf(long longerValue) {
-      return new BigInteger(EInteger.FromInt64(longerValue));
- }
+    public static BigInteger valueOf(long longerValue) => new BigInteger(EInteger.FromInt64(longerValue));
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.bitLength"]/*'/>
-  public int bitLength() {
-return this.Ei.GetSignedBitLength();
- }
+    public int bitLength() => this.Ei.GetSignedBitLength();
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.Equals(System.Object)"]/*'/>
-  public override bool Equals(object obj) {
+    public override bool Equals(object obj) {
       var bi = obj as BigInteger;
       return (bi == null) ? false : this.Ei.Equals(bi.Ei);
-}
+    }
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.GetHashCode"]/*'/>
-  public override int GetHashCode() {
-      return this.Ei.GetHashCode();
- }
+    public override int GetHashCode() => this.Ei.GetHashCode();
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.toBytes(System.Boolean)"]/*'/>
-  public byte[] toBytes(bool littleEndian) {
-      return this.Ei.ToBytes(littleEndian);
- }
+    public byte[] toBytes(bool littleEndian) => this.Ei.ToBytes(littleEndian);
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.toRadixString(System.Int32)"]/*'/>
-  public string toRadixString(int radix) {
-      return this.Ei.ToRadixString(radix);
- }
+    public string toRadixString(int radix) => this.Ei.ToRadixString(radix);
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.ToString"]/*'/>
-  public override string ToString() {
-      return this.Ei.ToString();
-    }
+    public override string ToString() => this.Ei.ToString();
 
     /// <include file='../docs.xml'
     /// path='docs/doc[@name="M:PeterO.BigInteger.CompareTo(PeterO.BigInteger)"]/*'/>
-    public int CompareTo(BigInteger other) {
-      return this.Ei.CompareTo(other == null ? null : other.Ei);
-    }
+    public int CompareTo(BigInteger other) => this.Ei.CompareTo(other == null ? null : other.Ei);
   }
 }
--- src/PeterO.Cbor/Cbor/CBORDataUtilities.cs
+++ src/PeterO.Cbor/Cbor/CBORDataUtilities.cs
@@ -6,46 +6,38 @@ If you like this, you should donate to Peter O.
 at: http://peteroupc.github.io/
  */
 using System;
-using PeterO;
 using PeterO.Numbers;
 
 namespace PeterO.Cbor {
     /// <include file='../../docs.xml'
     /// path='docs/doc[@name="T:PeterO.Cbor.CBORDataUtilities"]/*'/>
-  public static class CBORDataUtilities {
+  public static class CBORDataUtilities
+  {
     private const int MaxSafeInt = 214748363;
 
     /// <include file='../../docs.xml'
     /// path='docs/doc[@name="M:PeterO.Cbor.CBORDataUtilities.ParseJSONNumber(System.String)"]/*'/>
-    public static CBORObject ParseJSONNumber(string str) {
-      return ParseJSONNumber(str, false, false);
-    }
+    public static CBORObject ParseJSONNumber(string str) => ParseJSONNumber(str, false, false);
 
     /// <include file='../../docs.xml'
     /// path='docs/doc[@name="M:PeterO.Cbor.CBORDataUtilities.ParseJSONNumber(System.String,System.Boolean,System.Boolean)"]/*'/>
-    public static CBORObject ParseJSONNumber(
-      string str,
-      bool integersOnly,
-      bool positiveOnly) {
-      return ParseJSONNumber(str, integersOnly, positiveOnly, false);
-    }
+    public static CBORObject ParseJSONNumber(string str, bool integersOnly, bool positiveOnly) => ParseJSONNumber(str, integersOnly, positiveOnly, false);
 
     /// <include file='../../docs.xml'
     /// path='docs/doc[@name="M:PeterO.Cbor.CBORDataUtilities.ParseJSONNumber(System.String,System.Boolean,System.Boolean,System.Boolean)"]/*'/>
-    public static CBORObject ParseJSONNumber(
-      string str,
-      bool integersOnly,
-      bool positiveOnly,
-      bool preserveNegativeZero) {
-      if (String.IsNullOrEmpty(str)) {
+    public static CBORObject ParseJSONNumber(string str, bool integersOnly, bool positiveOnly, bool preserveNegativeZero)
+    {
+      if (String.IsNullOrEmpty(str))
         return null;
-      }
+
       var offset = 0;
       var negative = false;
-      if (str[0] == '-' && !positiveOnly) {
+      if (str[0] == '-' && !positiveOnly)
+      {
         negative = true;
         ++offset;
       }
+
       var mantInt = 0;
       FastInteger2 mant = null;
       var mantBuffer = 0;
@@ -60,204 +52,262 @@ namespace PeterO.Cbor {
       FastInteger2 newScale = null;
       int i = offset;
       // Ordinary number
-      if (i < str.Length && str[i] == '0') {
+      if (i < str.Length && str[i] == '0')
+      {
         ++i;
         haveDigits = true;
-        if (i == str.Length) {
-          if (preserveNegativeZero && negative) {
-            return CBORObject.FromObject(
-             EDecimal.NegativeZero);
-          }
+
+        if (i == str.Length)
+        {
+          if (preserveNegativeZero && negative)
+            return CBORObject.FromObject(EDecimal.NegativeZero);
+
           return CBORObject.FromObject(0);
         }
-        if (!integersOnly) {
-          if (str[i] == '.') {
+
+        if (!integersOnly)
+        {
+          if (str[i] == '.')
+          {
             haveDecimalPoint = true;
             ++i;
-          } else if (str[i] == 'E' || str[i] == 'e') {
-            haveExponent = true;
-          } else {
-            return null;
           }
-        } else {
-          return null;
+          else if (str[i] == 'E' || str[i] == 'e')
+          {
+            haveExponent = true;
+          }
+          else
+            return null;
         }
+        else
+          return null;
       }
-      for (; i < str.Length; ++i) {
-        if (str[i] >= '0' && str[i] <= '9') {
+
+      for (; i < str.Length; ++i)
+      {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
           var thisdigit = (int)(str[i] - '0');
-          if (mantInt > MaxSafeInt) {
-            if (mant == null) {
+          if (mantInt > MaxSafeInt)
+          {
+            if (mant == null)
+            {
               mant = new FastInteger2(mantInt);
               mantBuffer = thisdigit;
               mantBufferMult = 10;
-            } else {
-              if (mantBufferMult >= 1000000000) {
+            }
+            else
+            {
+              if (mantBufferMult >= 1000000000)
+              {
                 mant.Multiply(mantBufferMult).AddInt(mantBuffer);
                 mantBuffer = thisdigit;
                 mantBufferMult = 10;
-              } else {
+              }
+              else
+              {
                 mantBufferMult *= 10;
                 mantBuffer = (mantBuffer << 3) + (mantBuffer << 1);
                 mantBuffer += thisdigit;
               }
             }
-          } else {
+          }
+          else
+          {
             mantInt *= 10;
             mantInt += thisdigit;
           }
           haveDigits = true;
-          if (haveDecimalPoint) {
+
+          if (haveDecimalPoint)
+          {
             haveDigitsAfterDecimal = true;
-            if (newScaleInt == Int32.MinValue) {
-newScale = newScale ?? (new FastInteger2(newScaleInt));
+            if (newScaleInt == Int32.MinValue)
+            {
+              newScale = newScale ?? (new FastInteger2(newScaleInt));
               newScale.AddInt(-1);
-            } else {
+            }
+            else
+            {
               --newScaleInt;
             }
           }
-        } else if (!integersOnly && str[i] == '.') {
-          if (!haveDigits) {
-            // no digits before the decimal point
+        }
+        else if (!integersOnly && str[i] == '.')
+        {
+          // no digits before the decimal point
+          if (!haveDigits)
             return null;
-          }
-          if (haveDecimalPoint) {
+
+          if (haveDecimalPoint)
             return null;
-          }
+
           haveDecimalPoint = true;
-        } else if (!integersOnly && (str[i] == 'E' || str[i] == 'e')) {
+        }
+        else if (!integersOnly && (str[i] == 'E' || str[i] == 'e'))
+        {
           haveExponent = true;
           ++i;
           break;
-        } else {
-          return null;
         }
+        else
+          return null;
       }
-      if (!haveDigits || (haveDecimalPoint && !haveDigitsAfterDecimal)) {
+
+      if (!haveDigits || (haveDecimalPoint && !haveDigitsAfterDecimal))
         return null;
-      }
-      if (mant != null && (mantBufferMult != 1 || mantBuffer != 0)) {
+
+      if (mant != null && (mantBufferMult != 1 || mantBuffer != 0))
         mant.Multiply(mantBufferMult).AddInt(mantBuffer);
-      }
-      if (haveExponent) {
+
+      if (haveExponent)
+      {
         FastInteger2 exp = null;
         var expInt = 0;
         offset = 1;
         haveDigits = false;
-        if (i == str.Length) {
+        if (i == str.Length)
           return null;
-        }
-        if (str[i] == '+' || str[i] == '-') {
-          if (str[i] == '-') {
+
+        if (str[i] == '+' || str[i] == '-')
+        {
+          if (str[i] == '-')
             offset = -1;
-          }
+
           ++i;
         }
-        for (; i < str.Length; ++i) {
-          if (str[i] >= '0' && str[i] <= '9') {
+
+        for (; i < str.Length; ++i)
+        {
+          if (str[i] >= '0' && str[i] <= '9')
+          {
             haveDigits = true;
             var thisdigit = (int)(str[i] - '0');
-            if (expInt > MaxSafeInt) {
-              if (exp == null) {
+
+            if (expInt > MaxSafeInt)
+            {
+              if (exp == null)
+              {
                 exp = new FastInteger2(expInt);
                 expBuffer = thisdigit;
                 expBufferMult = 10;
-              } else {
-                if (expBufferMult >= 1000000000) {
+              }
+              else
+              {
+                if (expBufferMult >= 1000000000)
+                {
                   exp.Multiply(expBufferMult).AddInt(expBuffer);
                   expBuffer = thisdigit;
                   expBufferMult = 10;
-                } else {
+                }
+                else
+                {
                   // multiply expBufferMult and expBuffer each by 10
                   expBufferMult = (expBufferMult << 3) + (expBufferMult << 1);
                   expBuffer = (expBuffer << 3) + (expBuffer << 1);
                   expBuffer += thisdigit;
                 }
               }
-            } else {
+            }
+            else
+            {
               expInt *= 10;
               expInt += thisdigit;
             }
-          } else {
-            return null;
           }
+          else
+            return null;
         }
-        if (!haveDigits) {
+
+        if (!haveDigits)
           return null;
-        }
-        if (exp != null && (expBufferMult != 1 || expBuffer != 0)) {
+
+        if (exp != null && (expBufferMult != 1 || expBuffer != 0))
           exp.Multiply(expBufferMult).AddInt(expBuffer);
-        }
-      if (offset >= 0 && newScaleInt == 0 && newScale == null && exp == null) {
+
+        if (offset >= 0 && newScaleInt == 0 && newScale == null && exp == null)
+        {
           newScaleInt = expInt;
-        } else if (exp == null) {
-newScale = newScale ?? (new FastInteger2(newScaleInt));
-          if (offset < 0) {
+        }
+        else if (exp == null)
+        {
+          newScale = newScale ?? (new FastInteger2(newScaleInt));
+          if (offset < 0)
+          {
             newScale.SubtractInt(expInt);
-          } else if (expInt != 0) {
+          }
+          else if (expInt != 0)
+          {
             newScale.AddInt(expInt);
           }
-        } else {
-newScale = newScale ?? (new FastInteger2(newScaleInt));
-          if (offset < 0) {
+        }
+        else
+        {
+          newScale = newScale ?? (new FastInteger2(newScaleInt));
+          if (offset < 0)
+          {
             newScale.Subtract(exp);
-          } else {
+          }
+          else
+          {
             newScale.Add(exp);
           }
         }
       }
-      if (i != str.Length) {
-        // End of the string wasn't reached, so isn't a number
+
+      // End of the string wasn't reached, so isn't a number
+      if (i != str.Length)
         return null;
-      }
-      if ((newScale == null && newScaleInt == 0) || (newScale != null &&
-                    newScale.Sign == 0)) {
+
+      if ((newScale == null && newScaleInt == 0) || (newScale != null && newScale.Sign == 0))
+      {
         // No fractional part
-        if (mant != null && mant.CanFitInInt32()) {
+        if (mant != null && mant.CanFitInInt32())
+        {
           mantInt = mant.AsInt32();
           mant = null;
         }
-        if (mant == null) {
+        if (mant == null)
+        {
           // NOTE: mantInt can only be 0 or greater, so overflow is impossible
 #if DEBUG
-          if (mantInt < 0) {
-            throw new ArgumentException("mantInt (" + mantInt +
-              ") is less than 0");
-          }
+          if (mantInt < 0)
+            throw new ArgumentException("mantInt (" + mantInt + ") is less than 0");
 #endif
 
-          if (negative) {
+          if (negative)
+          {
             mantInt = -mantInt;
-            if (preserveNegativeZero && mantInt == 0) {
-              return CBORObject.FromObject(
-                EDecimal.NegativeZero);
-            }
+            if (preserveNegativeZero && mantInt == 0)
+              return CBORObject.FromObject(EDecimal.NegativeZero);
           }
           return CBORObject.FromObject(mantInt);
-        } else {
+        }
+        else
+        {
           EInteger bigmant2 = mant.AsBigInteger();
-          if (negative) {
+          if (negative)
             bigmant2 = -(EInteger)bigmant2;
-          }
+
           return CBORObject.FromObject(bigmant2);
         }
-      } else {
-        EInteger bigmant = (mant == null) ? ((EInteger)mantInt) :
-          mant.AsBigInteger();
-        EInteger bigexp = (newScale == null) ? ((EInteger)newScaleInt) :
-          newScale.AsBigInteger();
-        if (negative) {
+      }
+      else
+      {
+        EInteger bigmant = (mant == null) ? ((EInteger)mantInt) : mant.AsBigInteger();
+        EInteger bigexp = (newScale == null) ? ((EInteger)newScaleInt) : newScale.AsBigInteger();
+        if (negative)
           bigmant = -(EInteger)bigmant;
-        }
+
         EDecimal edec;
-        edec = EDecimal.Create(
-          bigmant,
-          bigexp);
-        if (negative && preserveNegativeZero && bigmant.IsZero) {
+        edec = EDecimal.Create(bigmant, bigexp);
+        if (negative && preserveNegativeZero && bigmant.IsZero)
+        {
           EDecimal negzero = EDecimal.NegativeZero;
           negzero = negzero.Quantize(bigexp, null);
           edec = negzero.Subtract(edec);
         }
+
         return CBORObject.FromObject(edec);
       }
     }
